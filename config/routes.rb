d35bdbc918dcf2628ee0f284137c76e5f666d5f1
Mario Uher <uher.mario@gmail.com>
2011-07-16 09:15:28 +0200

    Implemented test case.

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,10 @@ TableInsideForms::Application.routes.draw do
 
   # See how all your routes lay out with "rake routes"
 
+  post '' => 'application#create', :as => 'submit'
+
+  root :to => 'application#new'
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
