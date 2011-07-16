4e8fab9fd18b2c072aae9f4d74572598f32d9236
Mario Uher <uher.mario@gmail.com>
2011-07-16 08:32:49 +0200

    FIrst commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TableInsideForms::Application.config.secret_token = '5e4fa5709133a3c935ca51319eff9fdbccd44e90da897300332967a9ee7de7c2a13af538262f11fc8f6d96b86eec3fcb14670bc0620a632f4c8cadfd304515fc'
